08f60d48d55b8a82f031b54ed80ac33ca4abddfa
Sara Cortez <131759998+SarinhaCortez@users.noreply.github.com>
2024-11-18 16:05:01 +0000

    Add files via upload

---
+++ tux2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#===========
+#
+#mac => 00:c0:df:13:20:1d
+#
+#===========
+
+ifconfig eth1 172.16.101.1 netmask 255.255.255.0
+
+
+#===========
+#
+#microtik
+#/admin
+#/system reset-configuration
+#
+#===========
