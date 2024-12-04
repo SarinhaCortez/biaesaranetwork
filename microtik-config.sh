44992e9c2889825e1c80c06aad6689b7ba47d541
Sara Cortez <131759998+SarinhaCortez@users.noreply.github.com>
2024-12-04 14:50:02 +0000

    Update microtik-config.sh

--- microtik-config.sh
+++ microtik-config.sh
@@ -1,61 +1,6 @@
 #!/bin/bash
 
 stty -F /dev/ttyS0 115200 cs8 -cstopb -parenb
-echo -e "/system reset-configuration\r\n" > /dev/ttyS0
-echo -e "y\r\n" > /dev/ttyS0
-
-sleep 60 # change ?
-
-echo -e "admin\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether1]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether2]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether3]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether4]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether5]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether6]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether7]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether8]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether9]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether3]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether11]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether12]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether13]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether14]\r\n" > /dev/ttyS0
-sleep 1
 
 echo -e "/interface bridge port remove [find interface=ether15]\r\n" > /dev/ttyS0
 sleep 1
@@ -63,25 +8,7 @@ sleep 1
 echo -e "/interface bridge port remove [find interface=ether16]\r\n" > /dev/ttyS0
 sleep 1
 
-echo -e "/interface bridge port remove [find interface=ether17]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether18]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether19]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether20]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether21]\r\n" > /dev/ttyS0
-sleep 1
-
 echo -e "/interface bridge port remove [find interface=ether22]\r\n" > /dev/ttyS0
-sleep 1
-
-echo -e "/interface bridge port remove [find interface=ether23]\r\n" > /dev/ttyS0
 sleep 1
 
 echo -e "/interface bridge port remove [find interface=ether24]\r\n" > /dev/ttyS0
